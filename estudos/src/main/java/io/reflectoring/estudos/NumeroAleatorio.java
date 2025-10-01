3db43a46650955ae3d5b12263fcdec2d47442e31
Peters <pedrojulio.barreto@gmail.com>
2025-09-30 23:46:24 -0300

    Update: Criando NumeroAleatorio

---
+++ estudos/src/main/java/io/reflectoring/estudos/NumeroAleatorio.java
@@ -0,0 +1,34 @@
+package io.reflectoring.estudos;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumeroAleatorio {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int numeroGerado = new Random().nextInt(100);
+
+        System.out.println("Tente acertar o número de 1 a 100, você tem 5 tentativas: ");
+
+        int num = 0;
+
+        for (int i = 0; i < 5; i++) {
+            num = scanner.nextInt();
+
+            if(num == numeroGerado) {
+                System.out.println("Você acertou o número!!!");
+                break;
+            } else if(num < numeroGerado) {
+                System.out.println("O número que digitou é menor que o número.");
+            }else{
+                System.out.println("O número que digitou é maior que o número.");
+            }
+        }
+
+        if(num != numeroGerado) {
+            System.out.println("Você errou o número, mais sorte da próxima vez!");
+        }
+
+    }
+}
